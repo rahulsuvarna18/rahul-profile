fe03a5f812df20441809a626c01856628be3d85c
Rahul Suvarna <rahulsuvarna18@gmail.com>
2019-09-17 04:01:23 +0800

    added eventlistener to the page

--- app/javascript/components/typed.js
+++ app/javascript/components/typed.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const mainContent = () => {
   new Typed('#typed', {
     strings: ["I am a full-stack developer", "I love technology", "And love building game-changing applications", "Hit me up for your freelance work"],
-    typeSpeed: 50,
+    typeSpeed: 40,
   });
 }
 
